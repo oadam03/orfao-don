88024768147855f14c7d7b4def7f3354926ce3aa
ryan-don31 <rjdon9@gmail.com>
2023-03-13 22:55:47 -0400

    Updating files

--- spamDetectorServer/src/main/java/com/spamdetector/service/SpamResource.java
+++ spamDetectorServer/src/main/java/com/spamdetector/service/SpamResource.java
@@ -7,30 +7,35 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import jakarta.ws.rs.core.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @Path("/spam")
 public class SpamResource {
 //    your SpamDetector Class responsible for all the SpamDetecting logic
     SpamDetector detector = new SpamDetector();
+    List<TestFile> data;
 
-
-    SpamResource(){
-//        TODO: load resources, train and test to improve performance on the endpoint calls
+    SpamResource() throws IOException, URISyntaxException {
         System.out.print("Training and testing the model, please wait");
-
-//      TODO: call  this.trainAndTest();
-
-
+        File maindirectory = new File("D:\\SoftwareDev\\assignment1\\spamDetectorServer\\src\\main\\resources\\data");
+        this.data = this.trainAndTest(maindirectory);
     }
+
     @GET
     @Produces("application/json")
     public Response getSpamResults() {
-//       TODO: return the test results list of TestFile, return in a Response object
-
-        return null;
+        JSONArray json = new JSONArray(data);
+        Response resp = Response.status(200)
+                .header("Content-Type","application/json")
+                .entity(json)
+                .build();
+        return resp;
     }
 
     @GET
@@ -38,8 +43,26 @@ public class SpamResource {
     @Produces("application/json")
     public Response getAccuracy() {
 //      TODO: return the accuracy of the detector, return in a Response object
+        // number of correct guesses/number of guesses
+        // (number of true positives + number of true negatives) / number of files
+        double truepos = 0;
+        double trueneg = 0;
+        for(TestFile file : data){
+            if(file.getSpamProbability() > 0.5 && file.getActualClass() == "spam") {
+                trueneg += 1.0;
+            }
+            else if(file.getSpamProbability() < 0.5 && file.getActualClass() == "ham"){
+                truepos += 1.0;
+            }
+        }
 
-        return null;
+        double accuracy = (truepos + trueneg) / data.size();
+
+        Response resp = Response.status(200)
+                .header("Content-Type","application/json")
+                .entity(accuracy)
+                .build();
+        return resp;
     }
 
     @GET
@@ -47,11 +70,24 @@ public class SpamResource {
     @Produces("application/json")
     public Response getPrecision() {
        //      TODO: return the precision of the detector, return in a Response object
+        // number of true positives / (number of false positives + number of true positives)
+        double truepos = 0;
+        for(TestFile file : data){
+            if(file.getSpamProbability() < 0.5 && file.getActualClass() == "ham"){
+                truepos += 1.0;
+            }
+        }
 
-        return null;
+        double precision = truepos / data.size();
+
+        Response resp = Response.status(200)
+                .header("Content-Type","application/json")
+                .entity(precision)
+                .build();
+        return resp;
     }
 
-    private List<TestFile> trainAndTest()  {
+    private List<TestFile> trainAndTest(File maindirectory)  {
         if (this.detector==null){
             this.detector = new SpamDetector();
         }
--- spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
+++ spamDetectorServer/src/main/java/com/spamdetector/util/SpamDetector.java
@@ -1,6 +1,7 @@
 package com.spamdetector.util;
 
 import com.spamdetector.domain.TestFile;
+import org.json.JSONArray;
 
 import java.io.*;
 import java.util.*;
@@ -12,6 +13,7 @@ import java.util.regex.Pattern;
  * You may create more methods to help you organize you strategy and make you code more readable
  */
 public class SpamDetector {
+
     public static String[] getWords(String line){
         String[] words = line.split("\\s");
         Pattern wordreg = Pattern.compile("(?<![A-Za-z])[A-Za-z]+(?![A-Za-z])");
@@ -32,7 +34,104 @@ public class SpamDetector {
 
         return temp;
     }
+    public static Map<String,Double> buildProbs(int len, Map<String,Integer> map){
+        double size = (double)len;
+        Map<String,Double> Pr = new HashMap<>();
+        double prob = 0.0;
 
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String word = entry.getKey();
+            int freq = entry.getValue();
+            prob = freq / size;
+            Pr.put(word,prob);
+        }
+        return Pr;
+    }
+    public static File[] concatenate(File[] a, File[] b) {
+        int aLen = a.length;
+        int bLen = b.length;
+        File[] c = new File[aLen + bLen];
+        System.arraycopy(a, 0, c, 0, aLen);
+        System.arraycopy(b, 0, c, aLen, bLen);
+        return c;
+    }
+    public static Map<String,Integer> getFreq(File[] file){
+        if(file == null){
+            return null;
+        }
+        BufferedReader reader = null;
+        Map<String, Integer> frequency = new HashMap<>();
+
+        for(File hamchild : file) {
+            try {
+                reader = new BufferedReader(new FileReader(hamchild));
+                String line = reader.readLine();
+                while (line != null) {
+                    String[] words = getWords(line);
+
+                    for (String word : words) {
+                        if (frequency.containsKey(word)) {
+                            frequency.put(word, frequency.get(word) + 1);
+                        } else {
+                            frequency.put(word, 1);
+                        }
+                    }
+                    line = reader.readLine();
+                }
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return frequency;
+    }
+    public static Map<String, Double> getPrSW(Map<String,Double> PrWS, Map<String,Double>PrWH){
+        Map<String, Double> denom = new HashMap<>();
+        Map<String, Double> PrSW = new HashMap<>();
+
+        // Iterate through the keys in hashMap1
+        for (String key : PrWS.keySet()) {
+            // If the key is also in hashMap2, add its value to the sharedHashMap
+            if (PrWH.containsKey(key)) {
+                double value1 = PrWS.get(key);
+                double value2 = PrWH.get(key);
+                double totalValue = value1 + value2;
+                PrSW.put(key, PrWS.get(key) / totalValue);
+            }
+        }
+        return PrSW;
+    }
+    public static TestFile isFileSpam(Map<String,Double> PrSW, File file, String type){//type = spam or ham
+        if(file == null){
+            return null;
+        }
+        double n = 0.0;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line = reader.readLine();
+            while (line != null) {
+                String[] words = getWords(line);
+
+                for (String word : words) {
+                    if(PrSW.get(word) != null) {
+                        n += Math.log(1 - PrSW.get(word)) - Math.log(PrSW.get(word));
+                    }
+                }
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        double PrSF = 1/(1+(Math.pow(Math.E,n)));
+        TestFile testfile = new TestFile(file.getName(),PrSF,type);
+        return testfile;
+    }
+
+//    public static String createJSON()
     public static List<TestFile> trainAndTest(File mainDirectory) {
 //        TODO: main method of loading the directories and files, training and testing the model
 
@@ -46,56 +145,42 @@ public class SpamDetector {
         // PrWH = ham files containing word / number of ham files
 
         // put all PrSW into a treemap indexed by the word
-        File[] ham = (new File(mainDirectory + "\\ham\\")).listFiles();
-        File[] ham2 = (new File(mainDirectory + "\\ham2\\")).listFiles();
-        File[] spam = (new File(mainDirectory + "\\spam\\")).listFiles();
+        File[] ham1 = (new File(mainDirectory + "\\train\\ham")).listFiles();
+        File[] ham2 = (new File(mainDirectory + "\\train\\ham2")).listFiles();
 
-        Map<String, Integer> trainHamFreq = new HashMap<>();
-        Map<String, Integer> trainSpamFreq = new HashMap<>();
+        File[] ham = concatenate(ham1,ham2);
+        File[] spam = (new File(mainDirectory + "\\train\\spam")).listFiles();
 
-        Scanner scanner = null;
-        BufferedReader reader = null;
+        Map<String, Integer> trainHamFreq = getFreq(ham);
+        Map<String, Integer> trainSpamFreq = getFreq(spam);
 
-        for(File hamchild : ham){
-            try{
-                reader = new BufferedReader(new FileReader(hamchild));
-                String line = reader.readLine();
+        Map<String, Double> PrWS = buildProbs(spam.length, trainSpamFreq);
+        Map<String, Double> PrWH = buildProbs(ham.length, trainHamFreq);
+        Map<String, Double> PrSW = getPrSW(PrWS, PrWH);
 
-                while (line != null) {
-                    line = reader.readLine();
+        File[] testham = (new File(mainDirectory + "\\test\\ham")).listFiles();
+        File[] testspam = (new File(mainDirectory + "\\test\\spam")).listFiles();
 
-                    System.out.println(line);
+        ArrayList<TestFile> testfiles = new ArrayList<>();
 
-                    String[] words = getWords(line);
-
-                    for(String word : words){
-                        if(trainHamFreq.containsKey(word)){
-                            trainHamFreq.put(word,trainHamFreq.get(word) + 1);
-                        }else{
-                            trainHamFreq.put(word,1);
-                        }
-                    }
-                    /*
-                    * TODO:
-                    *  - split each line into words, checking if each word is valid (using regex)
-                    *  -
-                    * */
-                }
-                reader.close();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-
+        for(File hamfile : testham){
+            testfiles.add(isFileSpam(PrSW,hamfile,"ham"));
+        }
+        for(File spamfile : testspam){
+            testfiles.add(isFileSpam(PrSW,spamfile,"spam"));
+        }
+        for(TestFile guy : testfiles){
+            System.out.println(guy.getSpamProbability());
         }
 
-        return new ArrayList<TestFile>();
+        return testfiles;
     }
 
-    //main class for debugging
     public static void main(String[] args){
-        File file = new File("D:\\SoftwareDev\\assignment1\\spamDetectorServer\\src\\main\\resources\\data\\train");
-        trainAndTest(file);
+        File maindirectory = new File("D:\\SoftwareDev\\assignment1\\spamDetectorServer\\src\\main\\resources\\data");
+        List<TestFile> data = trainAndTest(maindirectory);
+        JSONArray json = new JSONArray(data);
+        System.out.println(json);
     }
 }
 
